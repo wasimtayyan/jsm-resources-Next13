6b37263af3e59c1d3b289ba9fa33ef0dfee0cd46
wasim altayyan <wasimtayyan@gmail.com>
2023-09-29 19:49:26 +0300

    fix logo path

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
                 <Link href='/'>
                     <Image
-                        src='./jsm-logo.svg'
+                        src='/jsm-logo.svg'
                         alt='logo'
                         width={55}
                         height={40}
