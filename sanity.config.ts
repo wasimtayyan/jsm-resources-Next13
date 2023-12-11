59dd2ef13ca808645e9e0fb1e5206e7c7dcfac3c
wasim altayyan <wasimtayyan@gmail.com>
2023-12-11 14:35:12 +0300

    cloudflare

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,7 +8,7 @@ import { deskTool } from "sanity/desk";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
-import schemas from "./sanity/schemas";
+import schemas from "./sanity/schemas/index";
 
 export default defineConfig({
   basePath: "/studio",
